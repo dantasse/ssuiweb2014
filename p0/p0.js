dee84103147b804a9b5b13483f7fdbb155a6badb
Dan Tasse <dan.tasse@gmail.com>
2014-08-17 16:17:02 -0400

    add p0 files for assignment 1

---
+++ p0/p0.js
@@ -0,0 +1,67 @@
+
+/* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Title : Project 0: JavaScript exercises
+Author : 
+Description : Exercises for Project 0
+Created : 
+Modified :
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
+
+/*
+* Summary:      Adds a header element to the body of the html document with the specified level and content
+* Parameters:   content: the text content of the header
+*               level: the level of the header (can be from h1 to h5). If the value is not a valid number
+*               or the passed in parameter is too high, defaults to h3.
+* Return:       undefined (i.e. just type in 'return').
+*/
+function addHeader() {
+
+} // end addHeader
+
+/*
+* Function description.
+* You may need to add parameters to the function
+*/
+function fibonacci() {
+
+} // end fibonacci
+
+/*
+* Function description.
+* You may need to add parameters to the function
+*/
+function findNumbersInStr() {
+
+} // end findNumbersInStr
+
+/*
+* Function description.
+* You may need to add parameters to the function
+*/
+function map() {
+
+} // end map
+
+/*
+* Function description.
+* You may need to add parameters to the function
+*/
+function makeIterator() {
+
+} // end makeIterator
+
+/*
+* Function description
+*/
+function runTests(){
+
+} // end runTests
+
+// To run your tests when the window loads, uncomment the code below:
+/*
+window.onload = function () {
+    runTests();
+}
+*/
--- p0/project0.md
+++ p0/project0.md
@@ -10,8 +10,8 @@ The goal of this project is to give you practice with JavaScript by completing a
 ## Files Provided
 The following files are available here:
 
-- [p0.js](: javascript file that you will need to edit
-- p0.html: html file that loads the p0 javascript file for testing 
+- [p0.js](p0.js): javascript file that you will need to edit
+- [p0.html](p0.html): html file that loads the p0 javascript file for testing 
 
 ## Project Overview
 For this project, modify p0.js by adding the functions below. Each function should behave as specified. You should also add a test function called runTests() which tests the functions that you wrote. The functions you need to define are below. You can 'run' your javascript file using p0.html. See the very last few lines of p0.js to see how you can easily call runTests once the p0.html page has loaded. IMPORTANT: Make sure your function names are correct!
