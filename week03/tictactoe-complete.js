c4373ad51bad45c8564f096a0736d3b682037b97
Dan Tasse <dan.tasse@gmail.com>
2014-09-15 11:09:52 -0400

    week 3 notes, plus changes to week 2 I guess?

---
+++ week03/tictactoe-complete.js
@@ -0,0 +1,44 @@
+(function() {
+'use strict';
+
+var gameState = {
+  board: [['x','o','_'], ['_', '_', 'x'],['_', '_', '_']],
+  turn: 'x',
+  gameOver: false
+}
+
+var spotClickedGen = function(row, col) {
+  return function() {
+    if (gameState.board[row][col] !== '_') {
+      return; // Do nothing.
+    }
+    if (gameState.turn === 'x') {
+      gameState.board[row][col] = 'x';
+      gameState.turn = 'o';
+    } else {
+      gameState.board[row][col] = 'o';
+      gameState.turn = 'x';
+    }
+    // TODO Check if someone wins?
+    document.body.innerHTML = ''; // Not an awesome way to clear the board.
+    displayBoard(gameState);
+  }
+}
+
+var displayBoard = function(gameState) {
+  for (var i = 0; i < gameState.board.length; i++) {
+    var rowArr = gameState.board[i];
+    var div = document.createElement('div');
+    for (var j = 0; j < rowArr.length; j++) {
+      var span = document.createElement('span');
+      span.innerText = rowArr[j];
+      span.className = 'tictactoe_spot';
+      span.addEventListener('click', spotClickedGen(i, j));
+      div.appendChild(span);
+    }
+    document.body.appendChild(div);
+  }
+}
+displayBoard(gameState);
+
+})();
