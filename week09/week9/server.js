78fa3d87c1a205287a8abf08a53dcbac7666b1af
Dan Tasse <dan.tasse@gmail.com>
2014-10-14 16:23:15 -0400

    weeks 8 and 9, starter

---
+++ week09/week9/server.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello Web Lab');
+  res.end();
+}).listen(8888);
+
+console.log('Server running at http://localhost:8888');
